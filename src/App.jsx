885259d72cd78c6c8b1d2d3a986c4d6e22831f69
sahilsolodev <sahilsolodev@gmail.com>
2025-04-12 11:20:31 +0000

    base setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+    <div className="font-display px-8 py-8">
+      <header className="text-2xl/5 tracking-tighter font-semibold ">
+        Sahil<br />Solomon.
+      </header>
+
+      <section className="py-24">
+        p
+        <p className="text-6xl/13 font-semibold tracking-tighter text-center">
+          I’m Sahil, a Python developer with a creative mind.
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default App
